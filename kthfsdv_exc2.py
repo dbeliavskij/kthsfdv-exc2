d76061f79bf3543f79b6c20adc299f45be41ccf0
dbeliavskij <d.beliavskij@gmail.com>
2020-09-03 11:54:14 +0200

    plotting dinamycally not implemented

--- kthfsdv_exc2.py
+++ kthfsdv_exc2.py
@@ -1,15 +1,68 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
+import os
+import time
 
-fig = plt.figure()
-fig.suptitle("Empty figure")
-ax = fig.add_subplot(1,1,1)
-ax.set_title('Empty plot')
 
-t = np.array(np.linspace(0, 2, 100))
+class Function:
+    fig = plt.figure()
+    global ax
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title('Function = 3*Pi*exp(-5*sin(2*Pi*1*t))')
 
-l = lambda a : 5 * np.sin(2 * np.pi * 1 * a)
-h = 3 * np.pi * np.exp(-1 * l(t))
+    def __init__(self, plotting_mode='-d'):
+        self.plotting_mode = plotting_mode
+        os.system("clear")
+        if self.plotting_mode == '-d':
+            self.default_plot()
+        elif self.plotting_mode == '-s':
+            try:
+                self.static_plot()
+            except ValueError:
+                os.system("clear")
+                print('Wrong arguments passed')
+        elif self.plotting_mode == '-t':
+            self.time_plot()
+        else:
+            print('No such arguments')
 
-ax.plot(t, h)
-plt.show()
+    def update(self, t):
+            h = lambda a: 3 * np.pi *\
+                np.exp(-1 * 5 * np.sin(2 * np.pi * 1 * a))
+            return h(self.t)
+
+    def plot(self, t, h):
+        ax.plot(self.t, h)
+        plt.show()
+
+    def default_plot(self, t=np.array(np.linspace(0, 2, 100))):
+        self.t = t
+        h = self.update(self.t)
+        self.plot(self.t, h)
+
+    def static_plot(self):
+        print('Plotting with user-defined variables')
+        self.t = np.array(np.linspace(int(sys.argv[2]),
+                                      int(sys.argv[3]),
+                                      int(sys.argv[4])))
+        h = self.update(self.t)
+        self.plot(self.t, h)
+
+    def time_plot(self, t=np.array(np.linspace(0, 2, 100))):
+        self.t = t
+        h = self.update(self.t)
+        self.plot(self.t, h)
+        while True:
+            self.t = np.linspace(t[0]+1, t[-1]+1, 100)
+            h = self.update(self.t)
+            ax.clear()
+            ax.plot(self.t, h)
+            time.sleep(1.0)
+
+
+if __name__ == "__main__":
+    try:
+        prog = Function(sys.argv[1])
+    except IndexError:
+        prog = Function()
